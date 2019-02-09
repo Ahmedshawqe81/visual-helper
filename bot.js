e90b34dbc1eed1d191a760b67ba0ea2679cf74da
Ahmedshawqe81 <45295940+Ahmedshawqe81@users.noreply.github.com>
2019-02-09 17:46:51 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js")
+const client = new Discord.Client()
+const prefix = ('!')
+
+const d = new Discord.RichEmbed()
+.setDescription(`${prefix}help-system
+${prefix}help-music
+${prefix}help-giveaway
+${prefix}help-files`)
+.setTitle('Visual Studio Codes Helper V0.1')
+.setFooter('VS V0.1 , All Rights Reserved ')
+.setColor('Random')
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(`${prefix} + 'help'`))
+    message.channel.send(d) 
+    return;
+});
+
+
+client.login(process.env.BOT_TOKEN);
